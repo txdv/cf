85ae4e41057aac94286402a41c4a73bd5c249399
Andrius Bentkus <andrius.bentkus@gmail.com>
2024-01-21 14:53:24 +0200

    add running step

--- build.zig
+++ build.zig
@@ -18,7 +18,8 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
 
     const main_tests = b.addTest(.{ .root_source_file = .{ .path = "cf.zig" } });
+    const run_main_tests = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_main_tests.step);
 }
