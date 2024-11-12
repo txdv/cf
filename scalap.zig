00d6ccdb9708eb1740f7d07e1790d178076200b8
Andrius Bentkus <andrius.bentkus@gmail.com>
2024-11-12 07:59:09 +0200

    print symbol table

--- scalap.zig
+++ scalap.zig
@@ -59,6 +59,132 @@ fn readString(cp: *ConstantPool, index: u16) ?[]u8 {
     };
 }
 
+fn regenerateZero(src: []u8) usize {
+    var i: usize = 0;
+    var j: usize = 0;
+    const srclen = src.len;
+
+    while (i < srclen) {
+        const input = src[i] & 0xff;
+        if (input == 0xc0 and (i + 1 < srclen) and (src[i + 1] & 0xff == 0x80)) {
+            src[j] = 0x7f;
+            i += 2;
+        } else if (input == 0) {
+            src[j] = 0x7f;
+            i += 1;
+        } else {
+            src[j] = (input - 1);
+            i += 1;
+        }
+        j += 1;
+    }
+    return j;
+}
+
+fn decode7to8(src: []u8, srclen: usize) usize {
+    var i: usize = 0;
+    var j: usize = 0;
+    const dstlen = (srclen * 7 + 7) / 8;
+
+    while (i + 7 < srclen) {
+        var out: u8 = src[i];
+        var in: u8 = src[i + 1];
+        src[j] = out | ((in & 0x01) << 7);
+        out = in >> 1;
+        in = src[i + 2];
+        src[j + 1] = out | ((in & 0x03) << 6);
+        out = in >> 2;
+        in = src[i + 3];
+        src[j + 2] = out | ((in & 0x07) << 5);
+        out = in >> 3;
+        in = src[i + 4];
+        src[j + 3] = out | ((in & 0x0f) << 4);
+        out = in >> 4;
+        in = src[i + 5];
+        src[j + 4] = out | ((in & 0x1f) << 3);
+        out = in >> 5;
+        in = src[i + 6];
+        src[j + 5] = out | ((in & 0x3f) << 2);
+        out = in >> 6;
+        in = src[i + 7];
+        src[j + 6] = out | (in << 1);
+
+        i += 8;
+        j += 7;
+    }
+
+    if (i < srclen) {
+        var out: u8 = src[i];
+        if (i + 1 < srclen) {
+            var in: u8 = src[i + 1];
+            src[j] = out | ((in & 0x01) << 7);
+            j += 1;
+            out = in >> 1;
+            if (i + 2 < srclen) {
+                in = src[i + 2];
+                src[j] = out | ((in & 0x03) << 6);
+                j += 1;
+                out = in >> 2;
+                if (i + 3 < srclen) {
+                    in = src[i + 3];
+                    src[j] = out | ((in & 0x07) << 5);
+                    j += 1;
+                    out = in >> 3;
+                    if (i + 4 < srclen) {
+                        in = src[i + 4];
+                        src[j] = out | ((in & 0x0f) << 4);
+                        j += 1;
+                        out = in >> 4;
+                        if (i + 5 < srclen) {
+                            in = src[i + 5];
+                            src[j] = out | ((in & 0x1f) << 3);
+                            j += 1;
+                            out = in >> 5;
+                            if (i + 6 < srclen) {
+                                in = src[i + 6];
+                                src[j] = out | ((in & 0x3f) << 2);
+                                j += 1;
+                                out = in >> 6;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (j < dstlen) src[j] = out;
+    }
+
+    return dstlen;
+}
+
+fn readVarInt32(reader: anytype) !i32 {
+    var result: i32 = 0;
+    var shift: u5 = 0;
+
+    while (true) {
+        const b: u8 = try reader.readByte();
+        result |= (@as(i32, b) & 0x7f) << shift;
+        if ((b & 0x80) != 0x80) break;
+        shift += 7;
+    }
+
+    return result;
+}
+
+fn readVar(comptime t: anytype, reader: anytype) !t {
+    var result: t = 0;
+    var shift: u5 = 0;
+
+    while (true) {
+        const b: u8 = try reader.readByte();
+        result |= (@as(t, b) & 0x7f) << shift;
+        if ((b & 0x80) != 0x80) break;
+        shift += 7;
+    }
+
+    return result;
+}
+
 pub fn main() !void {
     const filename = Utils.getFilename();
 
@@ -74,7 +200,51 @@ pub fn main() !void {
     defer cf.deinit();
 
     if (findScalaSig(&cf)) |scalaSig| {
-        //std.debug.print("{s}\n\n", .{scalaSig});
         try Utils.printHex(scalaSig);
+
+        const len = regenerateZero(scalaSig);
+
+        const new_len = decode7to8(scalaSig, len);
+
+        try Utils.printHex(scalaSig[0..new_len]);
+
+        var stream2 = std.io.fixedBufferStream(scalaSig);
+        const reader2 = stream2.reader();
+
+        const major_version = try readVarInt32(reader2);
+        const minor_version = try readVarInt32(reader2);
+
+        const symbol_count = try readVarInt32(reader2);
+
+        std.debug.print("version = {}.{} symbol_count = {}\n", .{
+            major_version,
+            minor_version,
+            symbol_count,
+        });
+
+        var i: usize = 0;
+        while (i < symbol_count) {
+            std.debug.print("{X:0>4} ", .{
+                try stream2.getPos(),
+            });
+            try readHeader(i, reader2);
+            i += 1;
+        }
     }
+}
+
+fn readHeader(n: usize, reader: anytype) !void {
+    var header: u32 = 0;
+    var size: u32 = 0;
+
+    header = try readVar(u32, reader);
+    size = try readVar(u32, reader);
+
+    std.debug.print("{d:>4}. header = {d:>4}, size = {d:>4}\n", .{
+        n,
+        header,
+        size,
+    });
+
+    try reader.skipBytes(@intCast(size), .{});
 }
